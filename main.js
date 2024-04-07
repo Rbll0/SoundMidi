4bfd724561d56b05b5695f3472abe2f92a1868b2
Gustavo Rabelo <gurabelo09@gmail.com>
2024-04-07 14:44:27 -0300

    testing the buttons and creating a list of elements

--- main.js
+++ main.js
@@ -2,8 +2,5 @@ function playSound () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-
-
-
-document.querySelector('.tecla_pom').onclick = playSound;
+document.querySelectorAll('.tecla')
 
