c62524fefafed0d618a7ca0139be27923608b1da
Gustavo Rabelo <gurabelo09@gmail.com>
2024-04-07 17:29:43 -0300

    scrolling through the list

--- main.js
+++ main.js
@@ -2,5 +2,19 @@ function playSound () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+listaDeTeclas[0].onclick = playSound;
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length ){
+    listaDeTeclas[contador].onclick = playSound;
+
+    // contador = (contador + 1);
+
+    contador += 1;
+
+    console.log(contador);
+    
+}
